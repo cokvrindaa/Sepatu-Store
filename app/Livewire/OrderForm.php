b03220367644609b554af9734d50c922e405ad46
cokvrindaa <master.cokorda@gmail.com>
2026-09-23 18:25:51 +0800

    livewire promocode discount

--- app/Livewire/OrderForm.php
+++ app/Livewire/OrderForm.php
@@ -47,39 +47,124 @@ class OrderForm extends Component
     }
 
     // kita menggunakan fungsi bawaan dari livewire yakni updated disambung dengan nama model yakni Quantity
+    // wire:model.live.debounce.500ms="quantity"
     public function updatedQuantity()
     {
         $this->validateOnly('quantity', [
             // mengecek apakah qunatity yang di input dari pengguna itu melebih batas dari stok?
-            'quantity' => 'required|integer|min:1|max:'.$this->shoe->stock
+            'quantity' => 'required|integer|min:1|max:'.$this->shoe->stock,
         ], [
             // jika lebih dari batas tampilkan teks stok tak tersedia
-            "quantity.max" => 'Stok tak tersedia'
+            'quantity.max' => 'Stok tak tersedia',
         ]
         );
-        
+
         $this->calculateTotal();
     }
-    
-    protected function calculateTotal(): void {
+
+    protected function calculateTotal(): void
+    {
         $this->subTotalAmount = $this->shoe->price * $this->quantity;
         $this->grandTotalAmount = $this->subTotalAmount - $this->discount;
     }
 
     // ketika button dari blade.php di klik menggunakan wire:click
 
-    public function incrementQuantity() {
-        if ( $this->quantity < $this->shoe->stock ){
+    public function incrementQuantity()
+    {
+        if ($this->quantity < $this->shoe->stock) {
             $this->quantity++;
             $this->calculateTotal();
         }
     }
 
-    public function decrementQuantity(){
-        if($this->quantity > 1){
+    public function decrementQuantity()
+    {
+        if ($this->quantity > 1) {
             $this->quantity--;
             $this->calculateTotal();
         }
+    }
+
+    // diggunakan untuk update secara realtime , dari method bawaan livewire yakni updated
+    // fungsi ini dipanggil dari blade.php dengan wire:model.live.debounce.500ms="promoCode"
+
+    public function updatedPromoCode()
+    {
+        $this->applyPromoCode();
+    }
+
+    public function applyPromoCode()
+    {
+
+        // jika promoCode tidak tersedia
+        if (! $this->promoCode) {
+            $this->resetDiscount();
+
+            return;
+        }
+
+        // mengirimkan data ke order service
+        $result = $this->orderService->applyPromoCode($this->promoCode, $this->subTotalAmount);
+
+        // mengganti data frontend bedasarkan kondisi backend
+        if (isset($result['error'])) {
+            session()->flash('error', $result['error']);
+            $this->resetDiscount();
+        } else {
+            session()->flash('message', 'Kode promo tersedia!');
+            $this->discount = $result['discount'];
+            $this->calculateTotal();
+            $this->promoCodeId = $result['promoCodeId'];
+            $this->totalDiscountAmount = $result['discount'];
+        }
+    }
+
+    protected function resetDiscount()
+    {
+        $this->discount = 0;
+        $this->calculateTotal();
+        $this->promoCodeId = null;
+        $this->totalDiscountAmount = 0;
+    }
+
+    // validasi
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'quantity' => 'required|int|min:1|max:'.$this->quantity,
+        ];
+    }
+
+    // method ini akan dipanggil ke method submit
+    protected function gatherBookingData(array $validatedData): array
+    {
+        return [
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'grand_total_amount' => $this->grandTotalAmount,
+            'sub_total_amount' => $this->subTotalAmount,
+            'total_discount_amount' => $this->totalDiscountAmount,
+            'promo_code' => $this->promoCode,
+            'promo_code_id' => $this->promoCodeId,
+            'discount' => $this->discount,
+            'quantity' => $this->quantity,
+        ];
+    }
+    
+    // dipanggil oleh wire:submit.prevent="submit" di blade.php
+    public function submit()
+    {   
+        // melakukan pemanggilan method sebelumnya, dan mengirim data ke orderService dengan method updateCustomerData
+        // updateCustomerData  memanggil updateSessionData di Repository
+        // data akan simpan ke session 
+        $validatedData = $this->validate();
+        $bookingData = $this->gatherBookingData($validatedData);
+        $this->orderService->updateCustomerData($bookingData);
+
+        return redirect()->route('front.customer_data');
     }
 
     public function render()
--- resources/views/livewire/order-form.blade.php
+++ resources/views/livewire/order-form.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full max-w-2xl space-y-6">
     <img src="{{ Storage::url($shoe->photos()->latest()->first()->photo) }}" class="w-full max-w-[260px] h-[160px] object-contain mx-auto" alt="Nike Air Humara Shoes">
-    <form action="customer-data.html" class="bg-white rounded-3xl shadow border p-6 space-y-4" id="booking-form">
+    <form wire:submit.prevent="submit" class="bg-white rounded-3xl shadow border p-6 space-y-4" id="booking-form">
       <h2 class="text-xl font-bold">{{ $shoe->name }}</h2>
       <p class="text-gray-600">Rp. {{ number_format($shoe->price) }} • {{ $orderData['shoe_size'] }}</p>
       <div class="flex items-center text-yellow-400 mb-2"><span>★★★★☆</span></div>
@@ -31,19 +31,25 @@
 
         {{-- intergrasi livewire --}}
         <input type="hidden" wire:model.live.debounce.500ms="quantity" id="quantity" name="quantity" value="1" />
-
+        
         <button type="button" wire:click="incrementQuantity" class="bg-[#C5F277] text-white rounded-full w-8 h-8 flex items-center justify-center">+</button>
       </div>
-
+      
       <div class="flex items-center bg-white rounded-full border border-gray-200 px-4 py-2 focus-within:ring-2 focus-within:ring-[#FFC700]">
         <img src="{{ asset('assets/images/icons/discount-shape.svg') }}" class="w-5 h-5 mr-2" alt="promo">
         <input type="text" wire:model.live.debounce.500ms="promoCode"  name="promo" placeholder="Promo Code" class="w-full outline-none" id="promo-input">
       </div>
+      
+      @if (session()->has('error'))
+        <p class="text-sm font-semibold" style="color: red;">{{ session('error') }}</p>
+      @elseif (session()->has('message'))
+        <p class="text-sm font-semibold" style="color: green;">{{ session('message') }}</p>
+      @endif
 
       <p id="promo-msg" class="text-sm"></p>
       <hr class="border-gray-200">
       <div class="flex justify-between"><span>Sub Total</span><span id="total-price" class="font-bold">Rp {{ number_format($grandTotalAmount) }}</span></div>
-      <div class="flex justify-between"><span>Discount</span><span id="discount-value" class="font-bold ">- Rp {{ $discount }}</span></div>
+      <div class="flex justify-between"><span>Discount</span><span id="discount-value" class="font-bold ">- Rp {{ number_format($discount) }}</span></div>
     </form>
     <div class="fixed bottom-4 left-4 right-4 z-50 md:static">
       <div class="flex items-center justify-between rounded-full bg-[#2A2A2A] p-2.5 pl-6 shadow-2xl">
@@ -51,7 +57,7 @@
           <p class="font-extrabold text-lg text-white" id="grand-total">Rp {{ number_format($grandTotalAmount) }}</p>
           <p class="text-[10px] text-gray-400">Grand total</p>
         </div>
-        <button type="submit" form="booking-form" class="rounded-full px-6 py-3 bg-[#C5F277] font-extrabold text-sm text-black">
+        <button  form="booking-form" class="rounded-full px-6 py-3 bg-[#C5F277] font-extrabold text-sm text-black">
           Continue
         </button>
       </div>
